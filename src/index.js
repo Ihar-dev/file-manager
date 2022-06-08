6447e377b813e84e705f9bed3944010b6561c926
Ihar Kozik <ihar.kozik@gmail.com>
2022-06-08 22:17:38 +0300

    refactor: edit index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ import { getPath } from './fs/path.js';
 import { COMMANDS } from './consts/commands.js';
 import { ACTIONS } from './consts/actions.js';
 
+const printCommands = () => {
+  console.log('\nPlease, print commands and wait for results:');
+  Object.entries(COMMANDS).forEach(([key, value]) => {
+    console.log(`\x1b[33m${key} \x1b[32m ${value}`); 
+  });
+}
+
 const printCurrentDir = () => {
   currentDir = getPath(currentPathArr);
   console.log(`\x1b[37m\nYou are currently in ${currentDir}`);
@@ -17,17 +24,13 @@ console.log(`Welcome to the File Manager, ${userName}!`);
 let currentDir = getHomedir();
 let currentPathArr = getPathArr(currentDir);
 
+printCommands();
+printCurrentDir();
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-
-console.log('\nPlease, print commands and wait for results:');
-Object.entries(COMMANDS).forEach(([key, value]) => {
-  console.log(`\x1b[33m${key} \x1b[32m ${value}`); 
-});
-
-printCurrentDir();
 
 rl.on('line', line => {
   if (line === '.exit') rl.close();
@@ -39,7 +42,7 @@ rl.on('line', line => {
           if (currentPathArr.length > 1) currentPathArr = ACTIONS[action](currentPathArr);
           break;
         default:
-          
+
       } 
     } else console.log('\x1b[31mInvalid input');
     printCurrentDir();
