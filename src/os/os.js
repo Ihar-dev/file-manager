619b5265b3a84421bd188cf2cb8957514e4a45f5
Ihar Kozik <ihar.kozik@gmail.com>
2022-06-12 21:53:14 +0300

    feat: add cpus print

--- src/os/os.js
+++ src/os/os.js
@@ -6,6 +6,15 @@ export const osMethods = async mode => {
       const osEOL = JSON.stringify(os.EOL);
       console.log(`Default system End-Of-Line is ${osEOL}`);
       break;
+    case 'cpus':
+      const osCpus = os.cpus();
+      console.log(`Overall amount of CPUS is ${osCpus.length}`);
+      osCpus.forEach((proc, index) => {
+        console.log(`${index + 1}:`);
+        console.log(`Model: ${proc.model}`);
+        console.log(`Clock rate is ${proc.speed} GHz`);
+      })
+      break;
     default:
   } 
 }
