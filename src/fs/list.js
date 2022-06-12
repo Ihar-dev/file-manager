e7e727851bfebcebbcb706cd00aac6768f148af7
Ihar Kozik <ihar.kozik@gmail.com>
2022-06-12 17:05:20 +0300

    refactor: edit ls method

--- src/fs/list.js
+++ src/fs/list.js
@@ -2,28 +2,25 @@ import * as path from 'path';
 import { access, readdir, stat } from 'fs/promises';
 
 export const list = async currentDir => {
-  const printCurrentDir = (index, files) => {
-    if (index === files.length - 1) {
-      setTimeout(() => {
-        console.log(`\x1b[37m\nYou are currently in ${currentDir}`);
-      });
-    }
-  }
+  const printCurrentDir = () => console.log(`\x1b[37m\nYou are currently in ${currentDir}`);
 
   const listDirectory = async () => {
     const files = await readdir(currentDir);
-    files.forEach(async (file, index) => {
-      const filePath = path.join(currentDir, file);
-      try {
-        await access(filePath);
-        const stats = await stat(filePath);
-        (stats.isDirectory()) ? console.log(`\x1b[33m${file}`) : console.log(`\x1b[36m${file}`);
-        printCurrentDir(index, files);
-      } catch {
-        console.log(`\x1b[35m${file}`);
-        printCurrentDir(index, files);
-      } 
+    const promiseArr = files.map(file => { return new Promise(async (resolve, reject) => {
+        const filePath = path.join(currentDir, file);
+        try {
+          await access(filePath);
+          const stats = await stat(filePath);
+          (stats.isDirectory()) ? console.log(`\x1b[33m${file}`) : console.log(`\x1b[36m${file}`);
+          resolve();
+        } catch {
+          console.log(`\x1b[35m${file}`);
+          reject();
+        } 
+      });
     });
+    await Promise.allSettled(promiseArr);
+    printCurrentDir();
   }
 
   try {
